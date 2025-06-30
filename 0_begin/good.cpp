3485ed5015cf443d5aad658e1b821a0ea4588dea
Jisang Yun <journeyofbabo@gmail.com>
2025-06-30 12:44:51 +0900

    git ignore

---
+++ 0_begin/good.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "h" << std::endl;
+
+    return 0;
+}
---
+++ 0_begin/hello_world.cpp
@@ -0,0 +1,20 @@
+// Simple c++ porgram
+#include <iostream>
+#include <ostream>
+
+int main() {
+  std::cout << "Hello, world" << std::endl;
+  std::cout << "I am Jisang" << std::endl;
+
+  std::cout << "hi" << std::endl
+            << "my name is "
+            << "Psi" << std::endl;
+
+  int i;
+
+  for (i = 0; i < 10; i++) {
+    std::cout << i << std::endl;
+  }
+
+  return 0;
+}
