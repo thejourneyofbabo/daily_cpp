9e589ca37aa3f96cfbdb1b20e513f1ed786876b8
thejourneyofbabo <journeyofbabo@gmail.com>
2025-07-08 16:21:15 +0900

    add file writing

--- 0_begin/ch7_file_write.cpp
+++ 0_begin/ch7_file_write.cpp
@@ -4,11 +4,15 @@
 
 int main() {
   // Prepare to write file
-  std::ofstream out("test.txt");
+  /*std::ofstream out("test.txt");*/
+  std::ofstream out("test.txt", std::ios::app);
 
   std::string s;
+  /*if (out.is_open()) {*/
+  /*  out << "Let's write this!!";*/
+  /*}*/
   if (out.is_open()) {
-    out << "Let's write this!!";
+    out << "add this";
   }
 
   return 0;
